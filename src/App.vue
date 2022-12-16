8f36732c8e542f9cc73baaf2bca75461fd526167
Ibrahim <ibrachar68@gmail.com>
2022-12-16 11:21:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import MainList from './components/MainList.vue'
+import HeaderVue from './components/HeaderVue.vue'
+import Search from './components/Search.vue'
+export default {
+  components: {
+    MainList,
+    HeaderVue,
+    Search
+  }
+}
+</script>
+
+<template>
+  <header>
+    <HeaderVue />
+  </header>
+
+  <main>
+    <Search />
+    <MainList />
+  </main>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MainList.vue
@@ -0,0 +1,22 @@
+<script >
+import Card from './Card.vue'
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <Card />
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
